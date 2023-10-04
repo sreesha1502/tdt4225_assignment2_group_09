a8c189dde3b98b4e57cd02f734f45d2eda37ad69
sreesha1502 <145578336+sreesha1502@users.noreply.github.com>
2023-10-04 20:34:20 +0200

    get average file update

--- getAverage.py
+++ getAverage.py
@@ -11,33 +11,19 @@ class ExampleGetData:
         query = "SELECT * FROM %s  WHERE has_labels = 1 ORDER BY id DESC"
         self.cursor.execute(query % table_name)
         rows = self.cursor.fetchall()
-        # print("Data from table %s, raw format:" % table_name)
-        # print(rows)
-        # # Using tabulate to show the table in a nice way
-        # print("Data from table %s, tabulated:" % table_name)
-        # print(tabulate(rows, headers=self.cursor.column_names))
         return rows
 
     def fetch_activityData_ForUser(self, table_name, userId):
         query = "SELECT * FROM %s WHERE user_id = %s"
         self.cursor.execute(query % (table_name,userId))
         rows = self.cursor.fetchall()
-        # print("Data from table %s, raw format:" % table_name)
-        # print(rows)
-        # # Using tabulate to show the table in a nice way
-        # print("Data from table %s, tabulated:" % table_name)
-        # print(tabulate(rows, headers=self.cursor.column_names))
         return rows
 
     def fetch_tpData_ForUser(self, table_name, activityId):
         query = "SELECT * FROM %s WHERE activity_id = %s"
         self.cursor.execute(query % (table_name,activityId))
         rows = self.cursor.fetchall()
-        # print("Data from table %s, raw format:" % table_name)
-        # print(rows)
-        # # Using tabulate to show the table in a nice way
-        # print("Data from table %s, tabulated:" % table_name)
-        # print(tabulate(rows, headers=self.cursor.column_names))
+
         return rows
 def main():
         program = None
@@ -46,13 +32,17 @@ def main():
             users = program.fetch_Userdata_WithContition(table_name="User")
             row = []
             columns = ["user_Id", "Average_TrackPoint", "Minimum_TrackPoint", "Maximum_TrackPoint"]
+
+            # Get all the users who has the label
             for user in users:
                 totalTrackPoint = 0
                 numberOfTrackPoints = 0
                 activities = program.fetch_activityData_ForUser(table_name="Activity", userId=user[0])
+
+                # Loop to get the activities for each users
                 for activity in activities:
                     trackpoints = program.fetch_tpData_ForUser(table_name="TrackPoint", activityId=activity[0])
-                    totalTrackPoint = totalTrackPoint + len(trackpoints)
+                    totalTrackPoint = totalTrackPoint + len(trackpoints) # count of tract points for each activity
                     if numberOfTrackPoints == 0:
                         minimumTP = len(trackpoints)
                         maximumTP = len(trackpoints)
@@ -64,8 +54,7 @@ def main():
                     numberOfTrackPoints = numberOfTrackPoints + 1
 
                     averageTP = totalTrackPoint / numberOfTrackPoints
-                print("userId", user[0],'averageTP',averageTP, 'minimumTP',minimumTP,'maximumTP', maximumTP)
-                row.append([user[0],averageTP,minimumTP,maximumTP])
+                row.append([user[0],averageTP,minimumTP,maximumTP])  # users with average minimum and maximum track points
             print(tabulate(row,headers=columns, tablefmt="grid"))
         except Exception as e:
             print("ERROR: Failed to use database:", e)
