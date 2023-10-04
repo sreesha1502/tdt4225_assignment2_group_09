a2af6da9afc45dc936737684c76f87ff6aa58310
sreesha1502 <145578336+sreesha1502@users.noreply.github.com>
2023-10-04 20:35:41 +0200

    part2 python files update

--- InvalidActivities.py
+++ InvalidActivities.py
@@ -1,10 +1,6 @@
-import datetime
-
 from DbConnector import DbConnector
 from tabulate import tabulate
-from math import radians, cos, sin, asin, sqrt
-from datetime import date
-import numpy
+
 class ExampleGetData:
 
     def __init__(self):
@@ -17,11 +13,6 @@ class ExampleGetData:
         query = "SELECT id, activity_id, date_time FROM TrackPoint WHERE activity_id = %i"
         self.cursor.execute(query % (activityId))
         rows = self.cursor.fetchall()
-        # print("Data from table %s, raw format:" % table_name)
-        # print(rows)
-        # # Using tabulate to show the table in a nice way
-        # print("Data from table %s, tabulated:" % table_name)
-        # print(tabulate(rows, headers=self.cursor.column_names))
         return rows
 
     def fetch_Activities(self):
@@ -29,24 +20,7 @@ class ExampleGetData:
         query = "SELECT id, user_id  FROM Activity"
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
-        # print("Data from table %s, raw format:" % table_name)
-        # print(rows)
-        # # Using tabulate to show the table in a nice way
-        # print("Data from table %s, tabulated:" % table_name)
-        # print(tabulate(rows, headers=self.cursor.column_names))
         return rows
-
-def mergeUsers(users):
-    merged_objects = {}
-    for obj in users:
-        key = obj['user']
-        if key not in merged_objects:
-            merged_objects[key] = obj.copy()
-        else:
-            merged_objects[key]['activity'] = obj['activity']
-
-    return list(merged_objects.values())
-
 
 def main():
     program = None
@@ -55,30 +29,29 @@ def main():
         activities = program.fetch_Activities()
         users = dict()
         columns = ["user", "Number of Invalid activities"]
-        numberActivitiesInvalid = 0
+
+        # loop through the activities table get each activity
         for activity in activities:
             trackpoints = program.fetch_TrackPoints(activityId= activity[0])
             currentTrackPoint = trackpoints[0]
-            invalid = 0
+
+            # loop through each trackpoint in an activity to get consequetive trackpoints
             for i in range(len(trackpoints)):
                 if (i != 0): currentTrackPoint = trackpoints[i-1]
                 nextTrackPoint = trackpoints[i]
 
-                # difference between datetime in timedelta
+                # difference between datetime in hours
                 delta = nextTrackPoint[2] - currentTrackPoint[2]
                 minutes = delta.total_seconds()/60
                 if (minutes > 5):
-                    print("user",activity[1],activity[0], minutes)
+                    print("user",activity[1],activity[0], minutes) # To print out the activities with user id and minute difference for each consequetive tractpoints with difference more than 5 minutes
                     if activity[1] in users.keys():
                         users[activity[1]] = users[activity[1]] + 1
                     else:
                         users[activity[1]] = 1
-        # result = mergeUsers(users)
-        print(users)
+        # print(users)
         my_users = [(u, users[u]) for u in users]
         print(tabulate(my_users, headers=columns))
-
-        #print(tabulate(users, headers=columns, tablefmt="grid"))
 
     except Exception as e:
         print("ERROR: Failed to use database:", e)
--- usersOrderByTransportation.py
+++ usersOrderByTransportation.py
@@ -1,6 +1,3 @@
-# SELECT user.id as userId, count(transportation_mode) as mode FROM User user,Activity aty WHERE user.id = aty.user_id AND aty.transportation_mode = 'bus' GROUP BY user.id;
-# SELECT User_id, transportation_mode, count(*) as amount FROM Activity GROUP BY user_id, transportation_mode;
-
 from DbConnector import DbConnector
 from tabulate import tabulate
 
@@ -14,45 +11,32 @@ class ExampleGetData:
         query = "SELECT  User_id, transportation_mode, count(*) as amount FROM %s WHERE transportation_mode = '%s' GROUP BY user_id, transportation_mode  ORDER BY amount desc"
         self.cursor.execute(query % (table_name,mode))
         rows = self.cursor.fetchall()
-        # print("Data from table %s, raw format:" % table_name)
-        # print(rows)
-        # # Using tabulate to show the table in a nice way
-        # print("Data from table %s, tabulated:" % table_name)
-        # print(tabulate(rows, headers=self.cursor.column_names))
         return rows
 
+ # This function is not referenced in the python code for reference purpose only
     def fetch_activityData_onTransportMode(self, table_name1,table_name2,transportMode):
         query = "SELECT user.id as userId, count(transportation_mode) as mode FROM %s user,%s aty WHERE user.id = aty.user_id AND aty.transportation_mode = %s GROUP BY user.id ORDER BY mode desc;"
         self.cursor.execute(query % (table_name1, table_name2,transportMode))
         rows = self.cursor.fetchall()
-        # print("Data from table %s, raw format:" % table_name)
-        # print(rows)
-        # # Using tabulate to show the table in a nice way
-        # print("Data from table %s, tabulated:" % table_name)
-        # print(tabulate(rows, headers=self.cursor.column_names))
         return rows
-
-    # take the second element for sort
-
-
-
-
 
 def main():
     program = None
 
     try:
         program = ExampleGetData()
-        transport_modes = ['walk', 'bike', 'bus', 'taxi', 'car','subway', 'train', 'airplane', 'boat', 'run','motorcycle']
+        transport_modes = ['walk', 'bike', 'bus', 'taxi', 'car','subway', 'train', 'airplane', 'boat', 'run', 'motorcycle']
+
+        # loop to get the users and transportation modes and the count of each transportation mode in decending order
         for tmode in transport_modes:
             print("Top ten users who use the", tmode)
             activities = program.fetch_activityData(table_name="Activity", mode=tmode)
             row = []
             columns = ["user_Id", "Amount"]
+
+            # Loop through the fetched data to get the first 10 results
             for activity in activities[:10]:
                 row.append([activity[0],activity[2]])
-                # print('user:', activity[0])
-                # print('number of transports by', tmode, ":", activity[2])
             print(tabulate(row, headers=columns, tablefmt="grid"))
     except Exception as e:
         print("ERROR: Failed to use database:", e)
